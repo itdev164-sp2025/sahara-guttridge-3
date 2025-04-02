24d1317d70e98e7a2459878244e391d61a8687f6
Sahara Guttridge <guttrids@gmatc.matc.edu>
2025-04-02 08:42:23 -0500

    pro3

--- blog/src/components/Header/Header.js
+++ blog/src/components/Header/Header.js
@@ -2,6 +2,7 @@ import * as React from "react"
 import { Link } from "gatsby"
 import styled from "styled-components"
 import { H1 } from "../Heading"
+import {Section} from "../Section"
 
 const StyledHeader = styled.header`
   margin: 0 auto;
@@ -20,11 +21,16 @@ const StyledLink = styled(Link)`
 
 const Header = ({ siteTitle }) => (
   <StyledHeader>
-    <StyledLink to="/">
+  <Section width={11/12}>
+  <StyledLink to="/">
     <H1> {siteTitle}</H1>
-  
-    </StyledLink>
+  </StyledLink>
+    </Section>
+    <Section width={1/12}>
+    Search
+    </Section>
   </StyledHeader>
 )
+
 
 export { Header}
