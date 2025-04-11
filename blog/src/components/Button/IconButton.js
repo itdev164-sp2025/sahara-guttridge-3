86719197e7e3de9bf731e63069034c234aab608d
Sahara Guttridge <guttrids@gmatc.matc.edu>
2025-04-10 20:14:51 -0500

    prgss2

---
+++ blog/src/components/Button/IconButton.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+import {BaseButton } from './BaseButton'
+
+const StyledButton = styled(BaseButton)`
+
+`
+
+export const IconButton = styled(({icon, ...rest}) =>{
+    let clone = React.cloneElement(icon, rest)
+    return <StyledButton {...rest} className={rest.className}>
+        {clone}
+    </StyledButton>
+})``
+
+IconButton.defaultProps = {
+    size: 24
+}
+
+IconButton.propTypes = {
+    icon: PropTypes.node.isRequired
+}
